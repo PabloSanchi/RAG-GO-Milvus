8ac87513a6313a031d54377748274360a122e2e6
Paul <pablo.sanchi.herrera@gmail.com>
2023-12-09 17:33:41 +0100

    feat: add milvus connection | implement create, delete a list collections

--- go.mod
+++ go.mod
@@ -13,10 +13,31 @@ require (
 require (
 	github.com/Boostport/migration v0.21.3 // indirect
 	github.com/apache/calcite-avatica-go/v3 v3.2.0 // indirect
+	github.com/cockroachdb/errors v1.9.1 // indirect
+	github.com/cockroachdb/logtags v0.0.0-20211118104740-dabe8e521a4f // indirect
+	github.com/cockroachdb/redact v1.1.3 // indirect
 	github.com/emicklei/go-restful/v3 v3.11.0 // indirect
+	github.com/getsentry/sentry-go v0.12.0 // indirect
 	github.com/gobuffalo/packr v1.19.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0 // indirect
 	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/milvus-io/milvus-proto/go-api/v2 v2.3.3 // indirect
 	github.com/milvus-io/milvus-sdk-go/v2 v2.3.3 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rogpeppe/go-internal v1.8.1 // indirect
+	github.com/tidwall/gjson v1.14.4 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	google.golang.org/genproto v0.0.0-20220503193339-ba3ae3f07e29 // indirect
+	google.golang.org/grpc v1.48.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
 )
--- repositories/datastore/milvus.go
+++ repositories/datastore/milvus.go
@@ -3,7 +3,11 @@ package datastore
 import (
 	"github.com/pablosanchi/datastore/core/domain"
 	"github.com/pablosanchi/datastore/core/ports"
+	"github.com/milvus-io/milvus-sdk-go/v2/client"
+	"github.com/milvus-io/milvus-sdk-go/v2/entity"
 	"fmt"
+	"context"
+	"log"
 )
 
 type DatastoreMilvus struct {
@@ -38,7 +42,9 @@ func (dsm *DatastoreMilvus) FromDomain(document *domain.Document) {
 	dsm.Embedding = []float32{} // here goes the embedding
 }
 
-
+var milvusClient, _ = client.NewClient(context.Background(), client.Config{
+	Address: "localhost:19530",
+})
 
 type DatastoreMilvusRepository struct {
 	
@@ -51,18 +57,112 @@ func NewDatastoreMilvusRepository() ports.DatastoreRepository {
 }
 
 func (m *DatastoreMilvusRepository) CreateCollection(collectionName string) error {
-	fmt.Printf("Creating collection | %s", collectionName)
+
+	schema := &entity.Schema{
+		CollectionName: collectionName,
+		Description:    "",
+		AutoID:         false,
+		Fields: []*entity.Field{
+			{
+				Name:       "id",
+				DataType:   entity.FieldTypeVarChar,
+				PrimaryKey: true,
+				AutoID:     false,
+				TypeParams: map[string]string{
+					entity.TypeParamMaxLength: fmt.Sprintf("%d", 255),
+				},
+			},
+			{
+				Name:       "title",
+				DataType:   entity.FieldTypeVarChar,
+				PrimaryKey: false,
+				AutoID:     false,
+				TypeParams: map[string]string{
+					entity.TypeParamMaxLength: fmt.Sprintf("%d", 255),
+				},
+			},
+			{
+				Name:       "content",
+				DataType:   entity.FieldTypeVarChar,
+				PrimaryKey: false,
+				AutoID:     false,
+				TypeParams: map[string]string{
+					entity.TypeParamMaxLength: fmt.Sprintf("%d", 3000),
+				},
+			},
+			{
+				Name:       "category",
+				DataType:   entity.FieldTypeVarChar,
+				PrimaryKey: false,
+				AutoID:     false,
+				TypeParams: map[string]string{
+					entity.TypeParamMaxLength: fmt.Sprintf("%d", 100),
+				},
+			},
+			{
+				Name:     "embedding",
+				DataType: entity.FieldTypeFloatVector,
+				TypeParams: map[string]string{
+					entity.TypeParamDim: fmt.Sprintf("%d", 768),
+				},
+			},
+		},
+	}
+
+	err := milvusClient.CreateCollection(
+		context.Background(),
+		schema,
+		entity.DefaultShardNumber,
+	)
+
+	if err != nil {
+		log.Fatal("failed to create collection:", err.Error())
+		return err
+	}
+
+	err = buildIndex(collectionName)
+
+	if err != nil {
+		log.Fatal("failed to build index:", err.Error())
+		return err
+	}
+
+	log.Println("collection created successfully")
+	
 	return nil
 }
 
 func (m *DatastoreMilvusRepository) DeleteCollection(collectionName string) error {
-	fmt.Println("Deleting collection", collectionName)
+	
+	err := milvusClient.DropCollection(
+		context.Background(),
+		collectionName,
+	)
+	
+	if err != nil {
+		log.Fatal("fail to drop collection:", err.Error())
+	}
+
+	log.Println("collection dropped successfully")
+
 	return nil
 }
 
 func (m *DatastoreMilvusRepository) List() ([]string, error) {
-	fmt.Println("Listing collections")
-	return []string{}, nil
+
+	listColl, err := milvusClient.ListCollections(context.Background(),)
+	if err != nil {
+		return nil, err
+	}
+
+	var collections []string
+	for _, collection := range listColl {
+		collections = append(collections, collection.Name)
+	}
+
+	log.Println("collections listed successfully")
+
+	return collections, nil
 }
 
 func (m *DatastoreMilvusRepository) UpsertDocuments(collectionName string, documents []domain.Document) error {
@@ -78,4 +178,32 @@ func (m *DatastoreMilvusRepository) UpsertDocuments(collectionName string, docum
 func (m *DatastoreMilvusRepository) Search(collectionName string, query string) ([]domain.Document, error) {
 	fmt.Printf("Searching documents in collection %s with query %s", collectionName, query)
 	return []domain.Document{}, nil
+}
+
+// private
+func buildIndex(collectionName string) error {
+	idx, err := entity.NewIndexIvfFlat(
+		entity.COSINE,
+		1024,
+	)
+
+	if err != nil {
+	  log.Fatal("fail to create ivf flat index parameter:", err.Error())
+	  return err
+	}
+
+	err = milvusClient.CreateIndex(
+		context.Background(),
+		collectionName,
+		"embedding",
+		idx,
+		false,
+	)
+	
+	if err != nil {
+		log.Fatal("fail to create index:", err.Error())
+		return err
+	}
+
+	return nil
 }
