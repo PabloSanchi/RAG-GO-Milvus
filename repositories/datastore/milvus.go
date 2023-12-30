264fa6b335513455b33eec6acfa7b1b2f6f35fe0
Paul <pablo.sanchi.herrera@gmail.com>
2023-12-30 02:47:25 +0100

    feat: add upsert and search features

--- repositories/datastore/milvus.go
+++ repositories/datastore/milvus.go
@@ -3,6 +3,7 @@ package datastore
 import (
 	"github.com/pablosanchi/datastore/core/domain"
 	"github.com/pablosanchi/datastore/core/ports"
+	"github.com/pablosanchi/datastore/core/ports/secondary"
 	"github.com/milvus-io/milvus-sdk-go/v2/client"
 	"github.com/milvus-io/milvus-sdk-go/v2/entity"
 	"fmt"
@@ -14,41 +15,18 @@ const (
     MilvusAddress = "localhost:19530"
 )
 
-type DatastoreMilvus struct {
-    ID        string
-    Title     string
-    Content   string
-    Category  string
-    Embedding []float32
-}
-
-func (dsm *DatastoreMilvus) ToDomain() *domain.Document {
-    return &domain.Document{
-        ID:       dsm.ID,
-        Title:    dsm.Title,
-        Content:  dsm.Content,
-        Category: dsm.Category,
-    }
-}
-
-func (dsm *DatastoreMilvus) FromDomain(document *domain.Document) {
-    dsm.ID = document.ID
-    dsm.Title = document.Title
-    dsm.Content = document.Content
-    dsm.Category = document.Category
-}
-
 type DatastoreMilvusRepository struct{
 	client client.Client
+	encoder secondary.TextEncoder
 }
 
-func NewDatastoreMilvusRepository() ports.DatastoreRepository {
+func NewDatastoreMilvusRepository(encoder secondary.TextEncoder) ports.DatastoreRepository {
     milvusClient, err := client.NewClient(context.Background(), client.Config{Address: MilvusAddress})
 	if err != nil {
 		log.Fatal("Failed to connect to Milvus:", err)
 	}
 
-	return &DatastoreMilvusRepository{client: milvusClient}
+	return &DatastoreMilvusRepository{client: milvusClient, encoder: encoder}
 }
 
 func (m *DatastoreMilvusRepository) CreateCollection(collectionName string) error {
@@ -90,21 +68,130 @@ func (m *DatastoreMilvusRepository) List() ([]string, error) {
 }
 
 func (m *DatastoreMilvusRepository) UpsertDocuments(collectionName string, documents []domain.Document) error {
-	fmt.Println("Upserting documents", collectionName)
+	nEntities := len(documents)
+	idList:= make([]string, 0, nEntities)
+	titleList:= make([]string, 0, nEntities)
+	contentList:= make([]string, 0, nEntities)
+	categoryList:= make([]string, 0, nEntities)
+	embeddingList := make([][]float32, 0, nEntities)
 
 	for _, document := range documents {
-		fmt.Println(document.String())
+		encodedContent, err := m.encoder.Encode(document.Content)
+
+		if err != nil {
+			log.Fatal("fail to encode content:", err.Error())
+		}
+		
+		idList = append(idList, document.ID)
+		titleList = append(titleList, document.Title)
+		contentList = append(contentList, document.Content)
+		categoryList = append(categoryList, document.Category)
+		embeddingList = append(embeddingList, encodedContent)
+	}
+
+	idColumn := entity.NewColumnVarChar("id", idList)
+	titleColumn := entity.NewColumnVarChar("title", titleList)
+	contentColumn := entity.NewColumnVarChar("content", contentList)
+	categoryColumn := entity.NewColumnVarChar("category", categoryList)
+	embeddingColumn := entity.NewColumnFloatVector("embedding", 4096, embeddingList)
+
+	if _, err := m.client.Upsert(
+		context.Background(), 
+		collectionName, 
+		"",
+		idColumn,
+		titleColumn,
+		contentColumn,
+		categoryColumn,
+		embeddingColumn,	
+	);
+
+	err != nil {
+			log.Fatalf("failed to upsert data, err: %v", err)
 	}
 
 	return nil
 }
 
 func (m *DatastoreMilvusRepository) Search(collectionName string, query string) ([]domain.Document, error) {
-	fmt.Printf("Searching documents in collection %s with query %s", collectionName, query)
-	return []domain.Document{}, nil
+
+	encodedQuery, err := m.encoder.Encode(query)
+
+	if err != nil {
+		log.Fatal("fail to encode query:", err.Error())
+	}
+
+	err = m.client.LoadCollection(
+		context.Background(),
+		collectionName,
+		false,
+	)
+
+	if err != nil {
+		log.Fatal("failed to load collection:", err.Error())
+	}
+
+	sp, _ := entity.NewIndexIvfFlatSearchParam( // NewIndex*SearchParam func
+		10,                                  // searchParam
+	)
+	
+	opt := client.SearchQueryOptionFunc(func(option *client.SearchQueryOption) {
+		option.Limit = 3
+		option.Offset = 0
+		option.ConsistencyLevel = entity.ClStrong
+		option.IgnoreGrowing = false
+	})
+
+	searchResult, err := m.client.Search(
+		context.Background(),
+		collectionName,
+		[]string{},
+		"",
+		[]string{"title", "content", "category"},
+		[]entity.Vector{entity.FloatVector(encodedQuery)},
+		"embedding",
+		entity.COSINE,
+		10,
+		sp,
+		opt,
+	)
+
+	if err != nil {
+		log.Fatal("fail to search collection:", err.Error())
+	}
+
+	fields := searchResult[0].Fields
+	titleList := fields.GetColumn("title")
+	contentList := fields.GetColumn("content")
+	categoryList := fields.GetColumn("category")
+
+	var documents []domain.Document
+	for i := 0; i < titleList.Len(); i++ {
+		
+		title, _ := titleList.GetAsString(i);
+		content, _ := contentList.GetAsString(i);
+		category, _ := categoryList.GetAsString(i);
+
+		documents = append(documents, domain.Document{
+			ID: "",
+			Title: title,
+			Content: content,
+			Category: category,
+		})
+	}
+	
+	err = m.client.ReleaseCollection(
+		context.Background(),                            // ctx
+		collectionName,                                          // CollectionName
+	)
+
+	if err != nil {
+		log.Fatal("failed to release collection:", err.Error())
+	}
+
+	return documents, nil
 }
 
-// private
 func (m *DatastoreMilvusRepository) buildIndex(collectionName string) error {
     idx, err := entity.NewIndexIvfFlat(entity.COSINE, 1024)
     if err != nil {
@@ -165,7 +252,7 @@ func defineSchema(collectionName string) *entity.Schema {
 				Name:     "embedding",
 				DataType: entity.FieldTypeFloatVector,
 				TypeParams: map[string]string{
-					entity.TypeParamDim: fmt.Sprintf("%d", 768),
+					entity.TypeParamDim: fmt.Sprintf("%d", 4096),
 				},
 			},
 		},
